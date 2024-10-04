f60450e2121289ce12909bde985a2a0a953d17bf
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-10-04 11:27:11 -0700

    finish

--- CPP/a02/a02.cpp
+++ CPP/a02/a02.cpp
@@ -1,13 +1,13 @@
 /**----------------------------------------------------------------------------
-   Author:        Kelvin Muturi
-   Date:        Octiber 6, 2024
-   Assignment:  CS161A Assignment a02
-   Description: This program reads the federal Withholding rate, FICA deduction
-                percentage
+   Author:      Kelvin Muturi
+   Date:        October 6, 2024
+   Assignment:  CS-161A Assignment a02
+   Description: This program calculates and outputs the weekly gross salary,
+                total deductions and the net salary.
    Inputs:      Employee ID number(integer), Number of hours worked(integer),
-                and hourly rate(double)
+                and hourly rate(double).
    Outputs:     gross pay, FICA deduction, federal tax withholding, total
-                deduction, and net pay (all as doubles)
+                deduction, and net pay (all as doubles).
    -----------------------------------------------------------------------****/
 
 #include <iostream>
@@ -16,12 +16,15 @@
 using namespace std;
 // function main
 int main() {
+    // welcome message
+    cout << "Welcome to my weekly salary Calculator!" << endl;
+
     // variables' declarations
     int empIdNum = 0;
     int numHrsWorked = 0;
     double hourlyRate = 0;
-    double fedWholdRate = 0.15;
-    const double FICADEDUCT = 0.0765; //constant declaration
+    double fedWholdRate = 0;
+    const double FICADEDUCT = 0.0765;
     double grossPay = 0;
     double ficaDedux = 0;
     double fedTaxWhold = 0;
@@ -29,12 +32,14 @@ int main() {
     double netPay = 0;
 
     // inputs
-    cout << "Enter the employee ID number: ";
+    cout << "\nEnter the employee ID number: ";
     cin >> empIdNum;
     cout << "Enter the number of hours worked: ";
     cin >> numHrsWorked;
     cout << "Enter the hourly rate: ";
     cin >> hourlyRate;
+    cout << "Enter the federal Tax withhold rate: ";
+    cin >> fedWholdRate;
 
     // calculations
     grossPay = numHrsWorked * hourlyRate;
@@ -44,12 +49,15 @@ int main() {
     netPay = grossPay - totalDedux;
 
     // outputs to the user
-    cout << "Your payroll summary:" << endl;
-    cout << "Gross amount paid is: " << fixed << setprecision(2) << grossPay << endl; // setting output to 2 decimal places
+    cout << "\nYour payroll summary:" << endl;
+    cout << "\nGross amount paid is: " << fixed << setprecision(2) << grossPay << endl; // setting output to 2 decimal places
     cout << "FICA deduction is: " << ficaDedux << endl;
     cout << "Federal Tax Withholding is: " << fedTaxWhold << endl;
     cout << "Total deductions: " << totalDedux << endl;
     cout << "Net Pay is: " << netPay << endl;
+
+    // goodbye
+    cout << "\nThank you. Goodbye!" << endl;
 
     return 0;
 }
