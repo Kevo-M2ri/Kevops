0469500a9e75921250a42edc8a33504e14215c6c
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-10-08 09:12:18 -0700

    UI improve and constant

--- CPP/a02/a02.cpp
+++ CPP/a02/a02.cpp
@@ -14,6 +14,10 @@
 #include <iomanip> //input and output manipulation library
 
 using namespace std;
+
+// constants declaration
+const double FICADEDUCT = 0.0765;
+
 // function main
 int main() {
     // welcome message
@@ -22,23 +26,22 @@ int main() {
     // variables' declarations
     int empIdNum = 0;
     int numHrsWorked = 0;
-    double hourlyRate = 0;
-    double fedWholdRate = 0;
-    const double FICADEDUCT = 0.0765;
-    double grossPay = 0;
-    double ficaDedux = 0;
-    double fedTaxWhold = 0;
-    double totalDedux = 0;
-    double netPay = 0;
+    double hourlyRate = 0.0;
+    double fedWholdRate = 0.0;
+    double grossPay = 0.0;
+    double ficaDedux = 0.0;
+    double fedTaxWhold = 0.0;
+    double totalDedux = 0.0;
+    double netPay = 0.0;
 
     // inputs
     cout << "\nEnter the employee ID number: ";
     cin >> empIdNum;
     cout << "Enter the number of hours worked: ";
     cin >> numHrsWorked;
-    cout << "Enter the hourly rate: ";
+    cout << "Enter the hourly rate: $";
     cin >> hourlyRate;
-    cout << "Enter the federal Tax withhold rate: ";
+    cout << "Enter the federal Tax withhold rate (enter percentage as decimal, for example: 17% = 0.17): ";
     cin >> fedWholdRate;
 
     // calculations
@@ -50,11 +53,11 @@ int main() {
 
     // outputs to the user
     cout << "\nYour payroll summary:" << endl;
-    cout << "\nGross amount paid is: " << fixed << setprecision(2) << grossPay << endl; // setting output to 2 decimal places
-    cout << "FICA deduction is: " << ficaDedux << endl;
-    cout << "Federal Tax Withholding is: " << fedTaxWhold << endl;
-    cout << "Total deductions: " << totalDedux << endl;
-    cout << "Net Pay is: " << netPay << endl;
+    cout << "\nGross amount paid is: $" << fixed << setprecision(2) << grossPay << endl; // setting output to 2 decimal places
+    cout << "FICA deduction is: $" << ficaDedux << endl;
+    cout << "Federal Tax Withholding is: $" << fedTaxWhold << endl;
+    cout << "Total deductions: $" << totalDedux << endl;
+    cout << "Net Pay is: $" << netPay << endl;
 
     // goodbye
     cout << "\nThank you. Goodbye!" << endl;
