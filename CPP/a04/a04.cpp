9c4ccedf189214940aa547f09a0df51dc3b9e671
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-10-18 12:00:09 -0700

    pseudo_a04

--- CPP/a04/a04.cpp
+++ CPP/a04/a04.cpp
@@ -2,15 +2,15 @@
    Author:      Kelvin Muturi
    Date:        October 17, 2024
    Assignment:  CS-161A Assignment a04
-   Description: This program shows the menu of product prices in a Ferry,
-                calculates and outputs the cost of an individual or a group of
-                less than 20 people, and gives a discount for next Ferry trip
-                for one adult if the price payed is $100 or more and offers
-                the remaining balance from obtaining a free ticket if bill is
-                less than 100.
+   Description: This program shows the menu of product prices in a Ferry for 
+                different groups of people, calculates and outputs the cost of
+                an individual or a group of less than 20 people, and gives a
+                discount for next Ferry trip for one adult if the price paid is
+                $100 or more and offers the remaining balance from obtaining a
+                free ticket if bill is less than 100.
    Inputs:      response as a character. numBikes, numAdults, numSeniors,
-                numYouths as integers
-   Outputs:     menu as a string, totalCharge as a double
+                numYouths as integers.
+   Outputs:     menu as a string, totalCharge, and discount as doubles.
    -----------------------------------------------------------------------****/
 
 #include <iostream>
@@ -34,6 +34,7 @@ int main () {
     int numYouths = 0;
     int allPeople = 0;
     double totalCharge = 0.0;
+    double discount = 0.0;
 
     // Welcome message
     cout << "Welcome to Washington State Ferries." << endl;
@@ -41,27 +42,28 @@ int main () {
 
     //menu header
     cout << setw(40) << left << "Fare Description";
-    cout << setw(30) << right << "Tickets ($)" << endl;
+    cout << setw(29) << right << "Tickets($)" << endl;
 
     // menu header/body separation
     cout << setfill('-') << setw(43) << left << "";
-    cout << setfill(' ') << setw(16) << right << "";
+    cout << setfill(' ') << setw(15) << right << "";
     cout << setfill('-') << setw(11) << right << "" << endl;
 
+    // setback whitespace as default space filler
     cout << setfill(' ');
 
     // menu body
     cout << fixed << setprecision(2);
     cout << setw(40) << left << "Vehicle Under 14\' (less than 168\") & Driver";
-    cout << setw(27) << right << VEHICLE_CHARGE << endl;
+    cout << setw(26) << right << VEHICLE_CHARGE << endl;
     cout << setw(40) << left << "Adult (ages 19 - 64)";
-    cout << setw(30) << right << ADULT_CHARGE << endl;
+    cout << setw(29) << right << ADULT_CHARGE << endl;
     cout << setw(40) << left << "Senior (ages 65 & over)";
-    cout << setw(30) << right << SENIOR_CHARGE << endl;
+    cout << setw(29) << right << SENIOR_CHARGE << endl;
     cout << setw(40) << left << "Youth (ages 6 - 18)";
-    cout << setw(30) << right << YOUTH_CHARGE << endl;
+    cout << setw(29) << right << YOUTH_CHARGE << endl;
     cout << setw(40) << left << "Bicycle Surcharge (included with Vehicle)";
-    cout << setw(29) << right << BIKE_SURCHARGE << endl << endl;
+    cout << setw(28) << right << BIKE_SURCHARGE << endl << endl;
 
     // inquiry_1
     cout << "Are you riding a vehicle on the Ferry (Y/N)? ";
@@ -80,7 +82,7 @@ int main () {
 
         // if branch_2
         if (allPeople > 20) {
-            cout << "\nUh oh!! Too many people in your group. Split into groups of at most 20 people and try again!"<< endl;
+            cout << "\nUh oh!! Too many people in your group. Split into groups of at most 20 people and try again!" << endl;
         }
         else {
             // if branch_3
@@ -102,10 +104,11 @@ int main () {
 
                 //if branch_4.2
                 if (totalCharge >= 100) {
-                    cout << "Congratulations! You have obtained a free adult ticket for your next trip."<< endl;
+                    cout << "Congratulations! You have obtained a free adult ticket for your next trip." << endl;
                 }
                 else {
-                    cout << "Spend $" << 100 - totalCharge << " more and get a FREE adult ticket for your next trip." << endl;
+                    discount = 100 - totalCharge;
+                    cout << "Spend $" << discount << " more and get a FREE adult ticket for your next trip." << endl;
                 }
             }
         }
@@ -115,7 +118,7 @@ int main () {
     }
 
     // goodbye message
-    cout << "\nThank you. Goodbye!" << endl;
+    cout << "\nThank you. Goodbye!!" << endl;
 
     return 0;
 }
