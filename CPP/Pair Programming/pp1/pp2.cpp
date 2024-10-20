c61ecded99e8988a1d4a6a5697c97cc4f3cd5219
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-10-20 14:33:02 -0700

    comments

--- CPP/Pair Programming/pp1/pp2.cpp
+++ CPP/Pair Programming/pp1/pp2.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 
 int main () {
+    // variable declarations
     int totalChange;
     int numDollars;
     int numQuarters;
@@ -21,63 +22,84 @@ int main () {
     int numNickels;
     int numPennies;
 
+    // input prompt
     cout << "Enter your change amount: ";
     cin >> totalChange;
 
+    // if change is 0
     if (totalChange == 0) {
         cout << "No change." << endl;
     }
+
+    // change is greater than 0
     else {
+        // if change is greater than 99
         if (totalChange >= 100) {
             numDollars = totalChange / 100;
             totalChange = totalChange % 100;
 
+            // output for 1
             if (numDollars == 1) {
                 cout << numDollars << " Dollar" << endl;
             }
+            // output for more than 1
             else if (numDollars > 1) {
                 cout << numDollars << " Dollars" << endl;
             }
         }
+        // change in the range 25 to 99
         if ((totalChange >= 25) && (totalChange < 100)) {
             numQuarters = totalChange / 25;
             totalChange = totalChange % 25;
 
+            // output for 1
             if (numQuarters == 1) {
                 cout << numQuarters << " Quarter" << endl;
             }
+            // output for more than 1
             else if (numQuarters > 1) {
                 cout << numQuarters << " Quarters" << endl;
             }
         }
+        // change in the range 10 to 24
         if ((totalChange >= 10) && (totalChange < 25)) {
             numDimes = totalChange / 10;
             totalChange = totalChange % 10;
 
+            // output for 1
             if (numDimes == 1) {
                 cout << numDimes << " Dime" << endl;
             }
+            // output for more than 1
             else if (numDimes > 1) {
                 cout << numDimes << " Dimes" << endl;
             }
         }
+
+        // change in the range 5 to 9
         if ((totalChange >= 5) && (totalChange < 10)) {
             numNickels = totalChange / 5;
             totalChange = totalChange % 5;
 
+            // output for 1
             if (numNickels == 1) {
                 cout << numNickels << " Nickel" << endl;
             }
+            // output for more than 1
             else if (numNickels > 1) {
                 cout << numNickels << " Nickels" << endl;
             }
         }
+
+        // change in the range 1 to 4
         if ((totalChange >=1 ) && (totalChange < 5)) {
             numPennies = totalChange;
 
+            // output for 1
             if (numPennies == 1) {
                 cout << numPennies << " Penny" << endl;
             }
+            // output for more than 1
             else if (numPennies > 1) {
                 cout << numPennies << " Pennies" << endl;
             }
