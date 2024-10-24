54f9f90836f37f927046d855eeb9ebd78a72a99c
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-10-23 20:12:20 -0700

    UPDATES

--- CPP/a05/a05.cpp
+++ CPP/a05/a05.cpp
@@ -6,28 +6,34 @@ using namespace std;
 int main () {
     string phrase1;
     string phrase2;
-    bool present = false;
 
-    cout << "Input your first phrase: ";
-    cin >> phrase1;
+    cout << "Hello! Welcome to search, find and output!!" << endl;
+
+    cout << "\nInput your first phrase: ";
+    getline(cin, phrase1);
     cout << "You have entered: " << phrase1 << endl;
     cout << "Input your second phrase: ";
-    cin >> phrase2;
-    cout << "You have entered: " << phrase2 << endl;
+    getline(cin, phrase2);
+    cout << "You have entered: " << phrase2 << "\n" << endl;
     
-    if (present != phrase1.find(phrase2)) {
-        cout << phrase1 << " is found in " << phrase2 << endl;
-    }
-
-    else if (present != phrase2.find(phrase1)) {
+    if (phrase1.find(phrase2) != string::npos) {
+        size_t position = phrase1.find(phrase2);
         cout << phrase2 << " is found in " << phrase1 << endl;
+        cout << phrase1.substr(position) << endl;
     }
-    else if ((present != phrase1.find(phrase2)) && (present != phrase2.find(phrase1))) {
+    else if (phrase2.find(phrase1) != string::npos) {
+        size_t position = phrase2.find(phrase1);
+        cout << phrase1 << " is found in " << phrase2 << endl;
+        cout << phrase2.substr(position) << endl;
+    }
+    else if (phrase1 == phrase2) {
         cout << "Both phrases match." << endl;
     }
     else {
         cout << "No match." << endl;
     }
+
+    cout << "\nThank you!! Goodbye!" << endl;
 
     return 0;
 }
