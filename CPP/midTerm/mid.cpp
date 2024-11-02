9347e3916c3ef2c3045accadd5434599e3676806
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-02 11:03:31 -0700

    finished

--- CPP/midTerm/mid.cpp
+++ CPP/midTerm/mid.cpp
@@ -1,25 +1,31 @@
 /****--------------------------------------------------------------------------
    Author:      Kelvin Muturi
-   Date:        November 1, 2024
+   Date:        November 2, 2024
    Assignment:  CS-161A Midterm
-   Description: This program asks the user to input two phrases, it checks
-                whether one phrase is in the other, outputs the phrase that is
-                in the other, and outputs the phrase containing the other from
-                the contained phrase. If the phrases are the same, it outputs
-                Both phrases match. If non matches the other, it outputs No
-                matches.
-   Inputs:      phrase1 and phrase2 as strings
-   Outputs:     phrasex is in phrasey, No matches, Both phrases match
+   Description: This program asks the user to input three names of contestants,
+                and the respective number of rocks each contestant has
+                collected. It then ranks the contestants according to the number
+                of rocks each has collected, lists them from the one with the
+                most to the one with the least. It the calculates and outputs
+                the average number of rocks collected as a double
+   Inputs:      contestant1, contestant2, and contestant3 as strings, and
+                numRocks1, numRocks2, and numRocks3 as integers
+   Outputs:     contestant1, contestant2, and contestant3 in order of one with
+                the most rocks to the one with the least, as strings, and
+                average number of rocks collected as a double
    -----------------------------------------------------------------------****/
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
+//constant declaration
 const int NUM_PLAYERS = 3;
 
 int main () {
+    //variables declaration
     string contestant1;
     string contestant2;
     string contestant3;
@@ -32,8 +38,10 @@ int main () {
     string third;
     double average;
 
+    // welcome message
     cout << "Hello!! Welcome to the winners program!!" << endl;
 
+    // contestants input prompt
     cout << "\nInput the first contestant's name: ";
     getline (cin, contestant1);
     cout << "Input the second contestant's name: ";
@@ -41,6 +49,7 @@ int main () {
     cout << "Input the third contestant's name: ";
     getline (cin, contestant3);
 
+    // number of rocks each contestant collects input prompt
     cout << "\nInput what " << contestant1 << " has: ";
     cin >> numRocks1;
     cout <<"Input what " << contestant2 << " has: ";
@@ -48,18 +57,23 @@ int main () {
     cout << "Input what " << contestant3 << " has: ";
     cin >> numRocks3;
 
+    // valid numbers for number of rocks
     if ((numRocks1 < 0) || (numRocks2 < 0) || (numRocks3 < 0)) {
         if ((numRocks = numRocks1) || (numRocks = numRocks2) || (numRocks = numRocks3)) {
+            // error message output and assigning
             cout << "Error!! Number of Rocks can only be at least!! try again!!" << endl;
             numRocks = 0;
         }
     }
     else {
+        // equal number of rocks for all
         if ((numRocks1 == numRocks2) && (numRocks2 == numRocks3)) {
-            first = contestant1.append(", ").append(contestant2).append(", and ").append(contestant3);
+            first = contestant1.append(", ").append(contestant2).append(", and ").append(contestant3); // listing all names
         }
         else {
+            // unequal number of rocks
             if ((numRocks1 != numRocks2) && (numRocks2 != numRocks3)) {
+                // obtaining largest to smallest
                 if ((numRocks1 > numRocks2) && (numRocks1 > numRocks3)) {
                     first = contestant1;
                     if (numRocks2 > numRocks3) {
@@ -94,7 +108,9 @@ int main () {
                     }
                 }
             }
+            // two contestants having equal number of rocks
             else if (numRocks1 == numRocks2) {
+                // obtaining the one(s) with the highest number of rocks
                 if (numRocks1 > numRocks3) {
                     first = contestant1.append(", and ").append(contestant2);
                     third = contestant3;
@@ -125,6 +141,7 @@ int main () {
                 }
             }
         }
+        // output conditions
         if ((numRocks1 != numRocks2) && (numRocks1 != numRocks3) && (numRocks2 != numRocks3)) {
             cout << "\nThe first winner is: " << first << endl;
             cout << "The second winner is: " << second << endl;
@@ -166,8 +183,15 @@ int main () {
                 }
             }
         }
+
+        // average number of rocks collected
+        average = static_cast<double>(numRocks1 + numRocks2 + numRocks3) / NUM_PLAYERS;
+
+        cout << fixed << setprecision(2); // setting output to 2 decimal places
+        cout << "\nThe average number of rocks collected is: " << average << endl;
     }
 
+    // goodbye message
     cout << "\nThank you! Have a good one!" << endl;
 
     return 0;
