c701f53d7a053dd839496855c589fd82edd68746
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-02 00:55:34 -0700

    running

--- CPP/midTerm/mid.cpp
+++ CPP/midTerm/mid.cpp
@@ -1,5 +1,6 @@
 /****--------------------------------------------------------------------------
-   Author:      November 1, 2024
+   Author:      Kelvin Muturi
+   Date:        November 1, 2024
    Assignment:  CS-161A Midterm
    Description: This program asks the user to input two phrases, it checks
                 whether one phrase is in the other, outputs the phrase that is
@@ -13,33 +14,34 @@
 
 #include <iostream>
 #include <string>
-#
 
 using namespace std;
+
+const int NUM_PLAYERS = 3;
 
 int main () {
     string contestant1;
     string contestant2;
     string contestant3;
     int numRocks;
-    string contestant;
     int numRocks1;
     int numRocks2;
     int numRocks3;
     string first;
     string second;
     string third;
+    double average;
 
     cout << "Hello!! Welcome to the winners program!!" << endl;
 
-    cout << "Input the first contestant's name: ";
+    cout << "\nInput the first contestant's name: ";
     getline (cin, contestant1);
     cout << "Input the second contestant's name: ";
     getline (cin, contestant2);
     cout << "Input the third contestant's name: ";
     getline (cin, contestant3);
 
-    cout << "Input what " << contestant1 << " has: ";
+    cout << "\nInput what " << contestant1 << " has: ";
     cin >> numRocks1;
     cout <<"Input what " << contestant2 << " has: ";
     cin >> numRocks2;
@@ -47,8 +49,10 @@ int main () {
     cin >> numRocks3;
 
     if ((numRocks1 < 0) || (numRocks2 < 0) || (numRocks3 < 0)) {
-        cout << "Error!! Number of Rocks cannot be negative!!";
-        numRocks = 0;
+        if ((numRocks = numRocks1) || (numRocks = numRocks2) || (numRocks = numRocks3)) {
+            cout << "Error!! Number of Rocks can only be at least!! try again!!" << endl;
+            numRocks = 0;
+        }
     }
     else {
         if ((numRocks1 == numRocks2) && (numRocks2 == numRocks3)) {
@@ -56,37 +60,115 @@ int main () {
         }
         else {
             if ((numRocks1 != numRocks2) && (numRocks2 != numRocks3)) {
-                if ((numRocks1 > numRocks2) && (numRocks2 > numRocks3)) {
+                if ((numRocks1 > numRocks2) && (numRocks1 > numRocks3)) {
                     first = contestant1;
-                    second = contestant2;
-                    third = contestant3;
+                    if (numRocks2 > numRocks3) {
+                        second = contestant2;
+                        third = contestant3;
+                    }
+                    else {
+                        second = contestant3;
+                        third = contestant2;
+                    }
                 }
-                else if ((numRocks2 > numRocks1) && (numRocks1 > numRocks3)) {
+                else if ((numRocks2 > numRocks1) && (numRocks2 > numRocks3)) {
                     first = contestant2;
-                    second = contestant1;
+                    if (numRocks1 > numRocks3) {
+                        second = contestant1;
+                        third =  contestant3;
+                    }
+                    else {
+                        second = contestant3;
+                        third = contestant1;
+                    }
+                }
+                else if ((numRocks3 > numRocks1) && (numRocks3 > numRocks2)) {
+                    first = contestant3;
+                    if (numRocks1 > numRocks2) {
+                        second = contestant1;
+                        third = contestant2;
+                    }
+                    else {
+                        second = contestant2;
+                        third = contestant1;
+                    }
+                }
+            }
+            else if (numRocks1 == numRocks2) {
+                if (numRocks1 > numRocks3) {
+                    first = contestant1.append(", and ").append(contestant2);
                     third = contestant3;
                 }
-                else if ((numRocks1 > numRocks3) && (numRocks3 > numRocks2)) {
-                    first = contestant1;
-                    second = contestant3;
+                else {
+                    first = contestant3;
+                    second = contestant1.append(", and ").append(contestant2);
+                }
+            }
+            else if (numRocks1 == numRocks3) {
+                if (numRocks1 > numRocks2) {
+                    first = contestant1.append(", and ").append(contestant3);
                     third = contestant2;
+                }
+                else {
+                    first = contestant2;
+                    second =contestant1.append(", and ").append(contestant3);
+                }
+            }
+            else if (numRocks2 = numRocks3) {
+                if (numRocks2 > numRocks1) {
+                    first = contestant2.append(", and ").append(contestant3);
+                    third = contestant1;
+                }
+                else {
+                    first = contestant1;
+                    second = contestant2.append(", and ").append(contestant3);
+                }
+            }
+        }
+        if ((numRocks1 != numRocks2) && (numRocks1 != numRocks3) && (numRocks2 != numRocks3)) {
+            cout << "\nThe first winner is: " << first << endl;
+            cout << "The second winner is: " << second << endl;
+            cout << "The third winner is: " << third << endl;
+        }
+        else {
+            if ((numRocks1 == numRocks2) && (numRocks2 == numRocks3)) {
+                cout << "\nIt\'s a tie! All are winners!" << endl;
+                cout << "The names are: " << first << endl;
+            }
+            else if ((numRocks1 == numRocks2) && (numRocks1 != numRocks3) && (numRocks2 != numRocks3)) {
+                if (numRocks2 > numRocks3) {
+                    cout << "\nIt\'s a tie!! The first winners are: " << first << endl;
+                    cout << "The third winner is: " << third << endl;
+                }
+                else {
+                    cout << "\nThe first winner is: " << first << endl;
+                    cout << "We have a tie! The second winners are: " << second << endl;
+                }
+            }
+            else if ((numRocks1 == numRocks3) && (numRocks1 != numRocks2) && (numRocks3 != numRocks2)) {
+                if (numRocks3 > numRocks2) {
+                    cout << "\nIt's a tie! The first winners are: " << first << endl;
+                    cout << "The third winner is: " << third << endl;
+                }
+                else {
+                    cout << "\nThe first winner is: " << first << endl;
+                    cout << "We have a tie! The second winners are: " << second << endl;
+                }
+            }
+            else if ((numRocks2 == numRocks3) && (numRocks2 != numRocks1) && (numRocks3 != numRocks1)) {
+                if (numRocks3 > numRocks1) {
+                    cout << "\nIt's a tie! The first winners are: " << first << endl;
+                    cout << "The third winner is: " << third << endl;
+                }
+                else {
+                    cout << "\nThe first winner is: " << first << endl;
+                    cout << "We have a tie! The second winners are: " << second << endl;
                 }
             }
         }
     }
 
-    if ((numRocks1 != numRocks2) && (numRocks1 != numRocks3) && (numRocks2 != numRocks3)) {
-        cout << "The first winner is: " << first << endl;
-        cout << "The second winner is: " << second << endl;
-        cout << "The third winner is: " << third << endl;
-    }
-    else {
-        if ((numRocks1 == numRocks2) && (numRocks2 == numRocks3)) {
-            cout << "It\'s a tie! All are winners!" << endl;
-            cout << "The names are: " << first << endl;
-        }
-    }
-        
+    cout << "\nThank you! Have a good one!" << endl;
 
     return 0;
 }
