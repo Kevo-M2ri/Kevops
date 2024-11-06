95aa59dd19239f12e0d7c72fa3fa99d2868d65a9
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-06 12:59:41 -0800

    random

--- CPP/starterDummy/StarterDummy.cpp
+++ CPP/starterDummy/StarterDummy.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
-int main () {
-    int highwayNumber;
+int main() {
+    int num;
+    cin >> num;
 
-    cin >> highwayNumber;
-
-    if ((highwayNumber > 0) && (highwayNumber <= 99)) {
-        if ((highwayNumber % 2 == 0) && (highwayNumber % 5 == 0)) {
-             cout << "I-" << highwayNumber << " is primary, going east/west.";
-        }
-        else if (highwayNumber % 5 == 0) {
-            cout << "I-" << highwayNumber << " is primary, going north/south.";
-        }
-    }
-    
-    else if ((highwayNumber > 100) && (highwayNumber <= 999)) {
-        if (highwayNumber % 100 == 0) {
-            cout << highwayNumber << " is not a valid interstate highway number.";
-        }
-        else if ((highwayNumber % 2 == 0) && (highwayNumber % 5 == 0)) {
-            cout << "I-" << highwayNumber << " is auxiliary, serving I-" << highwayNumber % 100 << ", going east/west.";
-        }
-        else if (highwayNumber % 5 == 0) {
-            cout << "I-" << highwayNumber << " is auxiliary, serving I-" << highwayNumber % 100 << ", going north/south.";
-        }
-    }
-
-    else {
-        cout << highwayNumber << " is not a valid interstate highway number.";
+    while (num > 0) {
+        cout << num % 2;
+        num = num / 2;
     }
     cout << endl;
 
