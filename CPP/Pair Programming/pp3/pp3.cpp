a488b1e6d06131c2722017147a2129dfa3c7a097
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-07 15:58:19 -0800

    Don't know what this is but,,,

---
+++ CPP/Pair Programming/pp3/pp3.cpp
@@ -0,0 +1,24 @@
+/****--------------------------------------------------------------------------
+   Author:      Kelvin Muturi
+   Date:        November 10, 2024
+   Assignment:  CS-161A Discussion 6
+   Description: This program asks the user for 
+   Inputs:      
+   Outputs:     
+   -----------------------------------------------------------------------****/
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main () {
+    int numInput;
+    char quit;
+
+    while (true){
+
+    }
+
+    return 0;
+}
