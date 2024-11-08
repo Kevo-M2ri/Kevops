ff0f08792ac9f4ee51f3ee82e51bca75c0633e32
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-08 11:58:25 -0800

    at least

--- CPP/Pair Programming/pp3/pp3.cpp
+++ CPP/Pair Programming/pp3/pp3.cpp
@@ -8,17 +8,47 @@
    -----------------------------------------------------------------------****/
 
 #include <iostream>
-#include <cctype>
+#include <iomanip> 
+#include <limits>
 
 using namespace std;
 
 int main () {
-    int numInput;
-    char quit;
+    int nums;
+    int count = 0;
+    int sum = 0;
+    double average = 0.0;
+    int maxNum = numeric_limits<int>::min();
+    int minNum = numeric_limits<int>::max();
+
+    cout << "Hello there!! Welcome to our simple statistics analysis  program!\n" << endl;
 
     while (true){
+        cout << "Enter a number. (Enter -1 to quit!): ";
+        cin >> nums;
 
+        if (nums == -1) {
+            break;
+        }
+        ++count;
+        sum += nums;
+        average = static_cast<double>(sum) / count;
+        if (nums > maxNum) {
+            maxNum = nums;
+        }
+        if (nums < minNum) {
+            minNum = nums;
+        }
     }
+
+    cout << fixed << setprecision(2);
+    cout << "\nYou entered: " << count << " digits" << endl;
+    cout << "The sum of the numbers is: " << sum << endl;
+    cout << "The average is: " << average << endl;
+    cout << "The largest number is: " << maxNum << endl;
+    cout << "The smallest number is: " << minNum << endl;
+
+    cout <<"\nGoodbye!! Thank you for using our program!!" << endl;
 
     return 0;
 }
