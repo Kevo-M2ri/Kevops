8ddc0be840509ff0fccfadd6b759bc2ae5b60d7b
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-08 13:31:45 -0800

    cooments and pseudo

--- CPP/Pair Programming/pp3/pp3.cpp
+++ CPP/Pair Programming/pp3/pp3.cpp
@@ -14,22 +14,28 @@
 using namespace std;
 
 int main () {
+    // variable declarations
     int nums;
     int count = 0;
     int sum = 0;
     double average = 0.0;
-    int maxNum = numeric_limits<int>::min();
-    int minNum = numeric_limits<int>::max();
+    int maxNum = numeric_limits<int>::min(); // initializing maxNum to the smallest number
+    int minNum = numeric_limits<int>::max(); // initializing minNum to the largest number
 
+    // Welcome message
     cout << "Hello there!! Welcome to our simple statistics analysis  program!\n" << endl;
 
+    // while
     while (true){
-        cout << "Enter a number. (Enter -1 to quit!): ";
+        cout << "Enter a number. (Enter -1 to quit!): "; // user input prompt
         cin >> nums;
 
+        // condition to quit
         if (nums == -1) {
             break;
         }
+
+        // calculations
         ++count;
         sum += nums;
         average = static_cast<double>(sum) / count;
@@ -41,13 +47,15 @@ int main () {
         }
     }
 
-    cout << fixed << setprecision(2);
+    // output messages
+    cout << fixed << setprecision(2); // set doubles to 2 decimal places
     cout << "\nYou entered: " << count << " digits" << endl;
     cout << "The sum of the numbers is: " << sum << endl;
     cout << "The average is: " << average << endl;
     cout << "The largest number is: " << maxNum << endl;
     cout << "The smallest number is: " << minNum << endl;
 
+    // goodbye message
     cout <<"\nGoodbye!! Thank you for using our program!!" << endl;
 
     return 0;
