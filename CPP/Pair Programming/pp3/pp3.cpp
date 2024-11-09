baa66160c7734491e73c3acb4826b6ed541b0b58
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-08 16:53:11 -0800

    update

--- CPP/Pair Programming/pp3/pp3.cpp
+++ CPP/Pair Programming/pp3/pp3.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <iomanip> 
 #include <limits>
+#include <cmath>
 
 using namespace std;
 
@@ -23,17 +24,19 @@ int main () {
     double average = 0.0;
     int maxNum = numeric_limits<int>::min(); // initializing maxNum to the smallest number
     int minNum = numeric_limits<int>::max(); // initializing minNum to the largest number
+    double variance = 0.0;
+    double standardDeviation = 0.0;
 
     // Welcome message
     cout << "Hello there!! Welcome to our simple statistics analysis  program!\n" << endl;
 
     // while
     while (true){
-        cout << "Enter a number. (Enter -1 to quit!): "; // user input prompt
+        cout << "Enter a number. (Enter a negative number to quit!): "; // user input prompt
         cin >> nums;
 
         // condition to quit
-        if (nums == -1) {
+        if (nums < 0) {
             break;
         }
 
@@ -47,6 +50,8 @@ int main () {
         if (nums < minNum) {
             minNum = nums;
         }
+        variance += (nums - average) * (nums - average) / count;
+        standardDeviation = sqrt(variance / count);
     }
 
     // output messages
@@ -56,6 +61,7 @@ int main () {
     cout << "The average is: " << average << endl;
     cout << "The largest number is: " << maxNum << endl;
     cout << "The smallest number is: " << minNum << endl;
+    cout << "The standard deviation is: " << standardDeviation << endl;
 
     // goodbye message
     cout <<"\nGoodbye!! Thank you for using our program!!" << endl;
