92f2aeda61fd4c6817be8af79fd5f9d8850695b1
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-17 09:44:24 -0800

    tgt

--- CPP/a07/a07.cpp
+++ CPP/a07/a07.cpp
@@ -9,6 +9,8 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cctype>
+#include <limits>
 
 using namespace std;
 
@@ -18,11 +20,15 @@ const double BEVERAGE_COST = 0.25;
 // function main
 int main () {
     // variables declaration
-    int coinVal = 0;
-    int allCoinsVal = 0;
-    double amountInDollars = 0.0;
-    char choice;
-    int numCups;
+
+    int coinVal = 0; // input for coins
+    int allCoinsVal = 0; // sum of coins
+    double amountInDollars = 0.0; // total amount in dollars
+    char choice = '\0'; // menu options
+    int numCups = 0;
+    double totalCost = 0.0; // items number
+    bool validChoice;
+    bool quitProgram = false;
 
     // welcome message
     cout << "Hello! Welcome to our coffee and tea dispenser!!\n" << endl;
@@ -32,43 +38,36 @@ int main () {
         cout << "Please input your coins - 5, 10, or 25 (0 to exit): ";
         cin >> coinVal;
         // inacceptable coins continuous skip condition
-        while ((coinVal != 5) || (coinVal != 10) || (coinVal != 25))
-        {
-            continue;
+        while (cin.fail() || (coinVal != 0 && coinVal != 5 && coinVal != 10 && coinVal != 25)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid coin! Please enter 5, 10, or 25 only (Enter 0 to finish): ";
+            cin >> coinVal;
         }
+
         allCoinsVal += coinVal; // test me
-    }
-    while (coinVal != 0); // condition to loop again
+    } while (coinVal != 0); // condition to loop again
 
     amountInDollars = static_cast<double>(allCoinsVal) / 100; // amount in dollars
     cout << fixed << setprecision(2) << endl; // two decimal places condition
     cout << "\nThe amount you entered is: $" << amountInDollars << endl;
 
     // menu
-    cout << "\nChoose your beverage ($" << BEVERAGE_COST << " each: )" << endl;
+    cout << "\nChoose your beverage ($" << BEVERAGE_COST << " each.): " << endl;
     cout << setw(5) << right << "C/c: Coffee" << endl;
     cout << setw(5) << right << "T/t: Tea" << endl;
     cout << setw(5) << right << "Q/q: Quit" << endl;
 
     // customer choices
-    while ((choice != 'q') || (choice != 'Q')) {
-        cin >> choice;
-        if ((choice == 'C') || (choice == 'c') || (choice = 't') || (choice == 'T')) {
-            cout <<  "How many cups would like? " << endl;
-            cin >> numCups;
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore();
-                cout << "Invalid option!!" << endl;
-                continue;
-           }
-        }
-        else {
-            cout << "Invalid choice! Please try again!" << endl;
-            continue;
-        }
+    while (!quitProgram) {
+        do {
+            cin >> choice;
+            validChoice = (tolower(choice) == 'c' || tolower(choice) == 't' || tolower(choice) == 'q');
+                if (!validChoice) {
+                    cout << "Invalid Option! Please choose a valid option!\n";
+            }
+        } while (!validChoice);
     }
-    
-
+        
     return 0;
 }
