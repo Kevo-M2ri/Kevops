2f5f62c83599571bedc7b8ac69305fc25ca71f28
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-24 12:26:31 -0800

    COMPLETE

--- CPP/a08/a08.cpp
+++ CPP/a08/a08.cpp
@@ -1,3 +1,16 @@
+/****--------------------------------------------------------------------------
+   Author:      Kelvin Muturi
+   Date:        November 24, 2024
+   Assignment:  CS-161A Assignment a08
+   Description: This program uses functions to ask the user to input the
+                number of assignments, and the scores for each assignment,
+                midterm and the finals and validates the input data. It then
+                calculates and outputs the final score for the student and
+                the grade of the student.
+   Inputs:      assignments as an iteger, score as a double
+   Outputs:     final score as a double, and final grade as a character
+   -----------------------------------------------------------------------****/
+
 #include <iostream>
 #include <limits>
 #include <string>
@@ -32,23 +45,20 @@ int main () {
     // banner
     Welcome();
 
+    //function calls
     numAssignments = ReadInt("\nInput the number of assignments completed (0-10): ");
-
     assignmentAverage = AssignAverage(numAssignments);
-
     midTermScore = ReadScore("\nEnter the midterm exam score: ");
     finalsScore = ReadScore("Enter the final exam score: ");
-
     allTermScore = CalcFinalScore(assignmentAverage, midTermScore, finalsScore);
-
     termGrade = CalcLetterGrade(allTermScore);
 
+    // program outputs
     cout << fixed << setprecision(1);
     cout << "\nThe score for the whole term is: " << allTermScore << endl;
     cout << "Your grade for the term is: " << termGrade << endl;
 
     cout << "\nThank you for using the calculator >>>> Goodbye!!" << endl;
-
 
     return 0;
 }
@@ -61,35 +71,36 @@ void Welcome() {
     cout << "Let's begin!!" << endl;
 }
 
-// readInt
+// read number of assignments function
 int ReadInt (string prompt) {
     int assignments;
     bool validInput = false;
 
     do {
-        cout << prompt;
+        cout << prompt; // user prompt for number of assignments
         cin >> assignments;
 
         if (cin.fail() || assignments < 0 || assignments > 10) {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Error!! Invalid input. Please enter a number btween 0 and 10\n";
+            cout << "Error!! Invalid input. Please enter a number between 0 and 10\n";
         }
         else {
             validInput = true;
         }
     }
-    while(!validInput);
+    while(!validInput); // validity of data inputted condition
 
     return assignments;
 }
 
+// read scores function
 double ReadScore(string prompt) {
     double score;
     bool validInput = false;
 
     do {
-        cout << prompt << endl;
+        cout << prompt << endl; //user prompt for scores per assignment
         cin >> score;
 
         if (cin.fail() || score < 0.0 || score > 4.0) {
@@ -101,11 +112,12 @@ double ReadScore(string prompt) {
             validInput = true;
         }
     }
-    while (!validInput);
+    while (!validInput); // validity of data inputted condition
 
     return score;
 }
 
+// average calcuclation funtion
 double AssignAverage (int numAssignments) {
     double average;
     double total = 0.0;
@@ -114,17 +126,18 @@ double AssignAverage (int numAssignments) {
     string prompt;
 
     if (numAssignments == 0) {
-        return 0.0;
+        return 0.0; //no assignments done
     }
     for (i = 1; i <= numAssignments; ++i) {
         prompt = "Enter score for assignment " + to_string(i) + ": ";
-        total += ReadScore(prompt);
+        total += ReadScore(prompt); //inputted scores function call
     }
     average = total / numAssignments;
 
     return average;
 }
 
+// final score calculation function
 double CalcFinalScore(double assignAvg, double midterm, double final) {
     double finalScore;
 
@@ -133,6 +146,8 @@ double CalcFinalScore(double assignAvg, double midterm, double final) {
     return finalScore;
 }
 
+
+// grade assigning function
 char CalcLetterGrade(double finalScore) {
     if (finalScore >= 3.3) {
         return 'A';
