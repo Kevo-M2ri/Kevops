dbc10fd6a460d236ed53457932042fbbbebc32dd
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-11-24 09:24:35 -0800

    cpp done

--- CPP/a08/a08.cpp
+++ CPP/a08/a08.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-const double ASSIGNMENT_WEIGHT = 0.6;
-const double MIDTERM_WEIGHT = 0.2;
-const double FINAL_WEIGHT = 0.2;
-
 // function prototypes
 void Welcome();
 int ReadInt(string prompt);
@@ -17,7 +13,15 @@ double AssignAverage(int numAssigns);
 double CalcFinalScore(double assignAvg, double midterm, double final);
 char CalcLetterGrade(double finalScore);
 
+// constants definitions
+const double ASSIGNMENT_WEIGHT = 0.6;
+const double MIDTERM_WEIGHT = 0.2;
+const double FINAL_WEIGHT = 0.2;
+
+// main function
 int main () {
+
+    // variables declarations
     int numAssignments;
     double assignmentAverage;
     double midTermScore;
@@ -25,13 +29,14 @@ int main () {
     double allTermScore;
     char termGrade;
 
+    // banner
     Welcome();
 
-    numAssignments = ReadInt("Input the number of assignments taken: ");
+    numAssignments = ReadInt("\nInput the number of assignments completed (0-10): ");
 
     assignmentAverage = AssignAverage(numAssignments);
 
-    midTermScore = ReadScore("Enter the midterm exam score: ");
+    midTermScore = ReadScore("\nEnter the midterm exam score: ");
     finalsScore = ReadScore("Enter the final exam score: ");
 
     allTermScore = CalcFinalScore(assignmentAverage, midTermScore, finalsScore);
@@ -39,10 +44,10 @@ int main () {
     termGrade = CalcLetterGrade(allTermScore);
 
     cout << fixed << setprecision(1);
-    cout << "The score for the whole term is: " << allTermScore << endl;
+    cout << "\nThe score for the whole term is: " << allTermScore << endl;
     cout << "Your grade for the term is: " << termGrade << endl;
 
-    cout << "Thank you for using the calculator >>>> Goodbye!!" << endl;
+    cout << "\nThank you for using the calculator >>>> Goodbye!!" << endl;
 
 
     return 0;
@@ -62,13 +67,13 @@ int ReadInt (string prompt) {
     bool validInput = false;
 
     do {
-        cout << "Input the number of assignments completed: ";
+        cout << prompt;
         cin >> assignments;
 
-        if (cin.fail() || assignments < 0) {
+        if (cin.fail() || assignments < 0 || assignments > 10) {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Error!! \nInvalid input. Please enter a valid non-negative integer!!\n";
+            cout << "Error!! Invalid input. Please enter a number btween 0 and 10\n";
         }
         else {
             validInput = true;
@@ -82,11 +87,9 @@ int ReadInt (string prompt) {
 double ReadScore(string prompt) {
     double score;
     bool validInput = false;
-    int count = 0;
-    int numAssigns = ReadInt("Number of Assignments"); 
 
-    while (count <= numAssigns) {
-        cout << "input your score for assignment " << count + 1 << ": " << endl;
+    do {
+        cout << prompt << endl;
         cin >> score;
 
         if (cin.fail() || score < 0.0 || score > 4.0) {
@@ -97,8 +100,8 @@ double ReadScore(string prompt) {
         else {
             validInput = true;
         }
-        ++count;
     }
+    while (!validInput);
 
     return score;
 }
@@ -107,9 +110,15 @@ double AssignAverage (int numAssignments) {
     double average;
     double total = 0.0;
     double score;
-    for (int i = 0; i < numAssignments; ++i) {
-        score =  ReadScore("Enter the score for the assignment (0-4.0): ");
-        total += score;
+    int i;
+    string prompt;
+
+    if (numAssignments == 0) {
+        return 0.0;
+    }
+    for (i = 1; i <= numAssignments; ++i) {
+        prompt = "Enter score for assignment " + to_string(i) + ": ";
+        total += ReadScore(prompt);
     }
     average = total / numAssignments;
 
@@ -125,16 +134,16 @@ double CalcFinalScore(double assignAvg, double midterm, double final) {
 }
 
 char CalcLetterGrade(double finalScore) {
-    if (finalScore >= 3.6) {
+    if (finalScore >= 3.3) {
         return 'A';
     }
-    else if (finalScore >= 3.0) {
+    else if (finalScore >= 2.8) {
         return 'B';
     }
     else if (finalScore >= 2.0) {
         return 'C';
     }
-    else if (finalScore >= 1.0) {
+    else if (finalScore >= 1.2) {
         return 'D';
     }
     else {
