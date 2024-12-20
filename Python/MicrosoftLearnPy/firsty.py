91c2f0f13d31416e9438d1fd8ee14a5fd44b9fde
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-12-19 18:28:01 -0800

    randompy

---
+++ Python/MicrosoftLearnPy/firsty.py
@@ -0,0 +1,17 @@
+#
+
+from datetime import date
+
+print(date.today())
+
+parsecs_input = input("Input number of parsecs: ")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
+
+object_size = 10
+if object_size > 5:
+    print('We need to keep an eye on this object')
+else:
+    print('Object poses no threat.')
