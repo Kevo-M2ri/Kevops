b0a3bc017510b0e0ed757a1cbb249a7cb3ebd88b
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2024-12-25 22:11:04 -0800

    a02py

--- Python/MicrosoftLearnPy/firsty.py
+++ Python/MicrosoftLearnPy/firsty.py
@@ -15,3 +15,15 @@ if object_size > 5:
     print('We need to keep an eye on this object')
 else:
     print('Object poses no threat.')
+
+text = """Interesting facts about the Moon. The Moon is Earth's only satelite. There are
+several interesting facts about the Moon and how it affects life here on Earth. On average, the
+Moon moves 4cm away from the Earth every year. This yearly drift is mot significant enough to
+cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
+
+sentences = text.split('. ')
+print (sentences)
+
+for sentence in sentences:
+    if 'temperature' in sentence:
+        print(sentence)
