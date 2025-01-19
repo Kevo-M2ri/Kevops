a42355730e3cd70295d3a7121880a7077bd968ab
Kevo-M2ri <138183587+Kevo-M2ri@users.noreply.github.com>
2025-01-19 12:54:07 -0800

    finish

--- CPP/161B/a02/a02.cpp
+++ CPP/161B/a02/a02.cpp
@@ -1,3 +1,18 @@
+/**----------------------------------------------------------------------------
+   Author:      Kelvin Muturi
+   Date:        January 19, 2025
+   Assignment:  CS-161B Assignment a02
+   Description: This program gives the user a menu with choices, allowing them
+                to pick a choice, then asks them some questions, giving them
+                results whilst checking the validity of their responses. It
+                then outputs an encoded file name made from both names, whether
+                assignment was submitted late, student ID, file name and time
+                of submission.
+   Inputs:      option, response, and colon as characters, fName, lName, stdID,
+                parseID, and fileName as character arrays, lateFlag as a 
+                boolean, hrs and mins as integers
+   Outputs:     encoded file name as a character array
+   -------------------------------------------------------------------------**/
 #include <iostream>
 #include <cstring>
 #include <cctype>
@@ -5,6 +20,7 @@
 #include <limits>
 using namespace std;
 
+// function prototypes
 void welcome();
 void displayMenu();
 void readOption(char &option);
@@ -15,8 +31,10 @@ void readTime(char strTime[]);
 void funcToLower(char myName[]);
 void goodbye();
 
+//constants declaration
 const int MAXCHAR = 20;
 
+// main function
 int main() {
     char choice;
     char encodedName[MAXCHAR * 5];
@@ -29,26 +47,30 @@ int main() {
 
         if (tolower(choice) == 'e') {
             encode(encodedName);
-            cout << "Encoded file name: " << encodedName << endl;
+            cout << "\nEncoded file name: " << encodedName << endl;
         }
     
-    } while (tolower(choice != 'q'));
+    } while (tolower(choice != 'q')); //quit clause
 
     goodbye();
 
     return 0;
 }
 
+// welcome message
 void welcome() {
     cout << "Welcome to the encoder..." << endl;
 }
 
+//menu options
 void displayMenu() {
-    cout << "You can choose to encode or quit." << endl;
+    cout << "\nYou can only choose to encode or quit." << endl;
     cout << "(E)ncode a file name" << endl;
     cout << "(Q)uit" << endl;
     cout << "Choose one of the choices to continue: " << endl;
 }
+
+//option input and validation
 void readOption(char &option) {
     bool validEntry = false;
 
@@ -63,7 +85,10 @@ void readOption(char &option) {
         }
     }
 }
+
+// encoding function
 void encode(char encodeFileName[]){
+    //variables declaration
     char fName[MAXCHAR];
     char lName[MAXCHAR];
     bool lateFlag;
@@ -71,19 +96,23 @@ void encode(char encodeFileName[]){
     char fileName[MAXCHAR];
     char strTime[MAXCHAR];
 
-    readInput(fName, lName, lateFlag);
-    readInput(parseID, fileName);
-    readTime(strTime);
+    
+    readInput(fName, lName, lateFlag); //names and late function call
+    readInput(parseID, fileName); // id and filename input fucntion call
+    readTime(strTime); //reading time of submision
 
-    strcpy(encodeFileName, lName);
+    //copying names into encodeFileName
+    strcpy(encodeFileName, fName);
     strcat(encodeFileName, "_");
-    strcat(encodeFileName, fName);
+    strcat(encodeFileName, lName);
     strcat(encodeFileName, "_");
 
+    //late condition
     if (lateFlag) {
         strcat(encodeFileName, "LATE_");
     }
 
+    // student ID and file name copying into encodeFileName
     strcat(encodeFileName, parseID);
     strcat(encodeFileName, "_");
     strcat(encodeFileName, strTime);
@@ -91,26 +120,28 @@ void encode(char encodeFileName[]){
     strcat(encodeFileName, fileName);  
 }
 
+// function to read user names and lateness in submission
 void readInput(char fName[], char lName[], bool &lateFlag) {
-    char response;
+    char response; // variable declaration
 
-    cout << "What is your first name? " << endl;
+    cout << "\nWhat is your first name? " << endl;
     cin >> fName;
-    funcToLower(fName);
+    funcToLower(fName); // convertion to lowercase
 
     cout << "What is your last name? " << endl;
     cin >> lName;
-    funcToLower(lName);
+    funcToLower(lName); //to lowercase
 
-    cout << "Was your assignement late (y/n)? ";
+    cout << "Was your assignment late (y/n)? ";
     cin >> response;
-    while (tolower(response) != 'y' && tolower(response) != 'n') {
+    while (tolower(response) != 'y' && tolower(response) != 'n') { //input validity condition
         cout << "Invalid input. Please enter 'y' or 'n': ";
         cin >> response;
     }
     lateFlag = (tolower(response) == 'y');
 }
 
+// student id and filename reading function
 void readInput(char parseID[], char fileName[]) {
     char stdID[MAXCHAR];
     cout << "Enter your student ID (xxx-xx-xxxx): ";
@@ -119,28 +150,30 @@ void readInput(char parseID[], char fileName[]) {
     while (strlen(stdID) != 11 || stdID[3] != '-' || stdID[6] != '-') {
         cout << "Invalid format! Enter student ID in the format xxx-xx-xxxx: ";
         cin >> stdID;
-    }
+    } //id format check
 
-    strncpy(parseID, stdID +7, 4);
+    strncpy(parseID, stdID + 7, 4); //copy the last 4 characters of stdID to parseID
     parseID[4] = '\0';
     cout << "Input file name: ";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cin.getline(fileName, MAXCHAR);
 }
 
+// time reading function
 void readTime(char strTime[]) {
+    //variable declaration
     int hrs = 0;
     char colon;
     int mins = 0;
 
-    cout << "Enter time in the HH:MM format: ";
+    cout << "Enter the time of submission(military time i.e. 13:00 for 1:00pm): ";
     cin >> hrs >> colon >> mins;
      while (colon != ':' || hrs < 0 || hrs > 23 || mins < 0 || mins > 59) {
         cout << "Invalid time format. Please enter time in HH:MM format: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cin >> hrs >> colon >> mins;
-    }
+    } //time format check
 
     snprintf(strTime, MAXCHAR, "%02d%02d", hrs, mins); // Format time as HHMM
 
@@ -149,12 +182,14 @@ void readTime(char strTime[]) {
     strcat(strTime, to_string(mins).c_str()); // convert mminutes into string then c_string and assign to strTime
 }
 
+//convert to lowercase function
 void funcToLower(char myName[]) {
     for (int i = 0; myName[i] != '\0'; i++) {
         myName[i] = tolower(myName[i]);
     }
 }
 
+// goodbye function
 void goodbye() {
-    cout << "Password Created successfully. Goodbye!" << endl;
+    cout << "\nPassword Created successfully. Goodbye!" << endl;
 }
