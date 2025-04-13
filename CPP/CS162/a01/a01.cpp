27534a0c393c3bab75779bfb39652e82c2751e7e
Kevo_M2ri <muturikelvinoficial@gmail.com>
2025-04-13 09:42:19 -0700

    Random

--- CPP/CS162/a01/a01.cpp
+++ CPP/CS162/a01/a01.cpp
@@ -23,16 +23,64 @@ struct Creature {
     int treasureCount;
 };
 
-Treasure randomTreasure();
-Creature randomCreature();
-bool doBattle(Creature &monster, int &playerHP, Treasure inventory[], int &inventoryCount);
-void printInventory(Treasure inventory[]);
+Creature randomEnemy();
+Creature makeSkeleton();
+Creature makeGoblin();
+Creature makeDragon();
+Creature makeBoss();
+void displayInventory(Treasure inventory[], int inventoryCount);
+int calculateTotalValue(Treasure inventory[], int inventoryCount);
+int performAttack(int attackType, Creature &enemy);
+void performEnemyAttack(Creature &enemy, int &playerHP);
+void addTreasure(Treasure newTreasure, Treasure inventory[], int &inventoryCount);
+void displayGameStatus(int playerHP, int enemyHP, Treasure inventory[], int inventoryCount);
+void clearScreen();
+void pressEnterToContinue();
+
 int main() {
     srand(time(nullptr));
 
     int playerHP = 50;
+    int playerAttack = 10;
     Treasure inventory[MAX_INVENTORY];
     int inventoryCount = 0;
+    int choice;
+    int attackChoice;
 
     return 0;
+}
+
+Creature randomEnemy() {
+    int enemyType = rand() % 4;
+    switch (enemyType) {
+        case 0:
+            return makeSkeleton();
+        case 1:
+            return makeGoblin();
+        case 2:
+            return makeDragon();
+        default:
+            return makeBoss();
+    }
+}
+
+Creature randomCreature() {
+    Creature c;
+    c.name = "Creature";
+    c.health = rand() % 100 + 1;
+    c.attack = rand() % 10 + 1;
+    return c;
+}
+
+bool doBattle(Creature &monster, int &playerHP, Treasure inventory[], int &inventoryCount) {
+    while (playerHP > 0 && monster.health > 0) {
+        playerHP -= monster.attack;
+        monster.health -= playerHP;
+    }
+}
+
+void printInventory(Treasure inventory[], int inventoryCount) {
+    for (int i = 0; i < inventoryCount; i++) {
+        cout << inventory[i].name << " " << inventory[i].value << endl;
+    }
 }
