88eced0a51515f11c13f5e3358e668447803dee3
Kelvin Muturi <muturikelvinofficial@gmail.com>
2025-04-20 16:42:13 -0700

    prac_1

---
+++ CPP/CS162/assessPrac_1/itemList.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "item.h"
+
+const int CAPACITY = 100;
+class ItemList
+{
+private:
+        InventoryItem   list[CAPACITY];
+        int             size;
+        int     capacity;
+
+public:
+        ItemList();
+        InventoryItem& get (int index);
+        void append(const InventoryItem& anItem);
+
+        void printList() const;
+        void readList(istream& in);
+};
---
+++ CPP/CS162/assessPrac_1/main.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include "item.h"
+#include "itemList.h"
+
+int main(int argc, char ** argv);
+
