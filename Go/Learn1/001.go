9d6313102ec1085bdedd1a6dd5e2710d6a818445
kevo-m2ri <muturikelvinofficial@gmail.com>
2026-02-17 14:00:58 -0800

    just started Golang go

---
+++ Go/Learn1/001.go
@@ -0,0 +1,11 @@
+package main //Package declaration
+
+import (
+	"fmt"
+	"math/rand"
+) //Import packages
+
+func main() { //Main function
+	fmt.Println("my favorite number is", rand.Intn(100)) //Statement to print a random number
+
+}
