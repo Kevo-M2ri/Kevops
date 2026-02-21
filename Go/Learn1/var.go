6faa66ea3dcc8687225becb3b9d7e05a0f5aa03c
Kevo-M2ri <muturikelvinofficial@gmail.com>
2026-02-20 21:26:54 -0800

    start

---
+++ Go/Learn1/var.go
@@ -0,0 +1,17 @@
+// var variableName type = value
+// variableName := value (can only be used inside a function)
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var student1 string = "John"
+	var stufent2 = "Jane"
+	x := 27
+
+	fmt.Println(student1)
+	fmt.Println(stufent2)
+	fmt.Println(x)
+}
