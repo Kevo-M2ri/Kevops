fea02b502d2b2b3878eae2a19a9abd686e8b2cc3
Kevo-M2ri <muturikelvinofficial@gmail.com>
2026-06-27 17:39:36 -0700

    start

--- C/celcius.c
+++ C/celcius.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 150
+#define STEP 15
+
 int main () {
     printf("Celsius to Fahrenheit conversion table\n");
     double celcius, fahrenheit;
-    double lower, upper, step;
 
-    lower = 0;
-    upper = 150;
-    step = 15;
-
-    celcius = lower;
+    
     printf("%s\t %s\n", "celcius", "fahrenheit");
-    while (celcius <= upper) {
+    for (celcius = LOWER; celcius <= UPPER; celcius += STEP) {
         fahrenheit = (celcius * 9 / 5) + 32;
         printf("%3.0f\t %6.1f\n", celcius, fahrenheit);
-        celcius = celcius + step;
+        
     }
 }
--- C/fahr.c
+++ C/fahr.c
@@ -11,7 +11,7 @@ int main() {
     step = 20; //step size
 
     fahr = lower;
-    printf("%s\t %s\n", "fahr", "celsius");
+    printf("%s\t %s\n", "celsius", "s");
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
         printf("%3.0f\t %6.1f\n", fahr, celsius);
