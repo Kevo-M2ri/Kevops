d16db9c8b78344257341b5ed86cde61bee515d64
Kevo-M2ri <muturikelvinofficial@gmail.com>
2026-07-04 21:48:26 -0700

    ass2

--- C/ass2/ass2.c
+++ C/ass2/ass2.c
@@ -1,40 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    //declarations
-    int frac_bits, exp_bits;
-    unsigned int hex_value;
-    unsigned int frac_mask;
-    unsigned int exp_mask;
-    unsigned int frac;
-    unsigned exp;
-    unsigned int sign;
-    int bias;
+int main(int argc, char* argv[]) {
+        //declarations
+        int frac_bits, exp_bits, bias;
+        unsigned int hex_value, frac_mask, exp_mask, fraction, exponent, sign;
+        double men, ele, total_value;
 
-    //checking for number of arguments
     if (argc != 4) {
-        printf("Usage: fp_parse <# frac_bits> <# of exp_bits> hex_to_convert\n");
+        printf("Usage: fp_parse <# of frac_bits> <# of exp_bits> <hex_to_convert>");
         return 1;
     }
 
-    //reading input from preexisting buffer
-    sscanf(argv[1], "%d", &frac_bits); // realized this from stack overflow
+    sscanf(argv[1], "%d", &frac_bits);
     sscanf(argv[2], "%d", &exp_bits);
+    sscanf(argv[3], "%d", &hex_value);
 
-    //checking for valid number of fraction bits
     if (frac_bits < 2 || frac_bits > 10) {
-        printf("Illegal number of fraction bits (11). Should be between 2 and 10");
+        printf("Illegal number of fraction bits (%d) .  Should be between 2 and 10", frac_bits);
         return 1;
     }
-
-    //checking for valid number of exponent bits
     if (exp_bits < 3 || exp_bits > 5) {
-        printf("Illegal number of expenent bits(8). Should be between 3 and 5");
+        printf("Illegal number of exponent bits (%d).   Should be between 3 and 5", exp_bits);
         return 1;
     }
 
+    frac_mask = (1 << frac_bits) - 1;
+    exp_mask = (1 << exp_bits) - 1;
 
+    fraction = hex_value & frac_mask;
+    exponent = (hex_value >> frac_bits) & exp_mask;
+    sign = (hex_value >> (frac_bits + exp_bits)) & 1;
 
+    bias = (1 << (exp_bits - 1)) - 1;
+
+    if (exponent == exp_mask) {
+        if (fraction == 0) {
+            if (sign) {
+                printf("-inf\n");
+            }
+            else {
+                printf("+inf\n");
+            }
+        }
+        else {
+            printf("NaN\n");
+        }
+    }
+    else {
+        if (exponent == 0) {
+            ele = 1 - bias;
+            men = (double)fraction / pow(2.0, frac_bits);
+        }
+        else {
+            ele = (int)exponent - bias;
+            men = 1.0 + ((double)fraction / pow(2.0, frac_bits));
+        }
+        total_value = men * pow(2.0, ele);
+        if (sign) {
+            total_value = -total_value;
+        }
+        printf("%f\n", total_value);
+    }
 }
